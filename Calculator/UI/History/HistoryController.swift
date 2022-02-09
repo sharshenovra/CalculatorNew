0af86c7b1fb6eb5ea2b642e9fc911890a9803659
sharshenovra <67783261+sharshenovra@users.noreply.github.com>
2022-02-09 10:39:43 +0600

    total

--- Calculator/UI/History/HistoryController.swift
+++ Calculator/UI/History/HistoryController.swift
@@ -10,86 +10,88 @@ import UIKit
 import SnapKit
 import RealmSwift
 
+// Создаём протокол(обязательное условие при его использовании) который будет содержать в себе одну обязательную при использовании функцию
 protocol HistorySelectDelegate: AnyObject {
-    func selectHistory(model: HistoryModel)
+    func selectHistory(model: HistoryModel) // Указываем функцию которая будет выполнять действие при выборе элемента в истории
 }
 
+// Создаём наш HistoryController для истории
 class HistoryController: UIViewController{
     
-    private lazy var historyTableView = HistoryTableView()
+    private lazy var historyTableView = HistoryTableView() // Создаём historyTableView из ранее созданной HistoryTableView
     
-    private lazy var clearButton = CustomButton(title: "Clear all")
-    private lazy var backButton = CustomButton(title: "Back")
+    private lazy var clearButton = CustomButton(title: "Clear all")  // Создаём clearButton(кнопку) из ранее созданной CustomButton с title "Clear all"
+    private lazy var backButton = CustomButton(title: "Back")  // Создаём backButton(кнопку) из ранее созданной CustomButton с title "Back"
     
-    private lazy var viewModel: HistoryViewModel = {
-        return HistoryViewModel(delegate: self)
+    private lazy var viewModel: HistoryViewModel = {  // Создаём переменную которая будет содержать в себе нашу модель для истории
+        return HistoryViewModel(delegate: self)  // Возвращаем нашу модель с использованием её делегата 
     }()
     
-    public static func newInstanse(delegate: HistorySelectDelegate) -> HistoryController {
-        let controller = HistoryController()
-        controller.viewModel.historyDelegate = delegate
-        return controller
+    public static func newInstanse(delegate: HistorySelectDelegate) -> HistoryController { // Создаём функцию которая принимаем в себе делегат и возвращает HistoryController
+        let controller = HistoryController() // Создаём переменную со значением нашего контроллера
+        controller.viewModel.historyDelegate = delegate // Указываем что в нашем контроллере делегат будет равен делегату который мы укажем при использовании функции
+        return controller // Возвращаем контроллер
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func viewDidLoad() { // Метод который используется при создании экрана
+        super.viewDidLoad() // Используем метод Swift
         
-        setupConstraints()
-        setupMainWindow()
-        setupViews()
+        setupConstraints() // Осуществляем реализацию функции описанной ниже
+        setupMainWindow() // Осуществляем реализацию функции описанной ниже
+        setupViews() // Осуществляем реализацию функции описанной ниже
         
-        viewModel.getHistory()
+        viewModel.getHistory() // Осуществляем реализацию функции описанной ниже
     }
     
-    private func setupMainWindow() {
-        view.backgroundColor = .black
+    private func setupMainWindow() { // Создаём функцию которая будет менять фон
+        view.backgroundColor = .black // Указываем что фон будет черный(.black)
     }
     
-    func setupViews(){
-        historyTableView.delegate = self
+    func setupViews(){ // Создаём функцию которая будет отвечать за параметры наших элементов
+        historyTableView.delegate = self // Указываем что наш tableView будет использовать собственный делегат
         
-        backButton.setOnClickListener { view in
-            self.navigationController?.popToRootViewController(animated: true)
+        backButton.setOnClickListener { view in  // Создаём действие для нашей кнопки backButton используя метод setOnClickListener прописанный в CustomButton
+            self.navigationController?.popToRootViewController(animated: true) // Указываем что будет совершен переход на исходный(Main) контроллер с анимацией
         }
         
-        clearButton.setOnClickListener { view in
-            self.viewModel.deleteAllHistory()
+        clearButton.setOnClickListener { view in  // Создаём действие для нашей кнопки clearButton используя метод setOnClickListener прописанный в CustomButton
+            self.viewModel.deleteAllHistory() // Указываем что при нажатии будет выполнена функция удаления всей истории ранее созданная в нашей HistoryModel
         }
     }
     
-    func setupConstraints(){
-        view.addSubview(clearButton)
-        clearButton.snp.makeConstraints { make in
-            make.top.equalTo(view.safeArea.top).offset(8)
-            make.right.equalToSuperview().offset(-8)
+    func setupConstraints(){ // Создаём функцию которая будет отвечать за разметку наших элементов
+        view.addSubview(clearButton) // Добавляем clearButton в наш контроллер
+        clearButton.snp.makeConstraints { make in // Задаем констрейнты
+            make.top.equalTo(view.safeArea.top).offset(8) // Указываем что верх нашей кнопки будет в безопасном месте сверху с отступом в 8 пикселей
+            make.right.equalToSuperview().offset(-8) // Указываем что справа она будет привязана к родителю с отступом в -8 пикселей
         }
         
-        view.addSubview(backButton)
-        backButton.snp.makeConstraints { make in
-            make.top.equalTo(view.safeArea.top).offset(8)
-            make.left.equalToSuperview().offset(8)
+        view.addSubview(backButton) // Добавляем clearButton в наш контроллер
+        backButton.snp.makeConstraints { make in // Задаем констрейнты
+            make.top.equalTo(view.safeArea.top).offset(8) // Указываем что верх нашей кнопки будет в безопасном месте сверху с отступом в 8 пикселей
+            make.left.equalToSuperview().offset(8) // Указываем что слева она будет привязана к родителю с отступом в 8 пикселей
         }
         
-        view.addSubview(historyTableView)
-        historyTableView.snp.makeConstraints { make in
-            make.width.equalToSuperview()
-            make.top.equalTo(clearButton.snp.bottom)
-            make.bottom.equalToSuperview()
+        view.addSubview(historyTableView) // Добавляем historyTableView в наш контроллер
+        historyTableView.snp.makeConstraints { make in // Задаем констрейнты
+            make.width.equalToSuperview() // Указываем что ширина нашего historyTableView будет равна родителю
+            make.top.equalTo(clearButton.snp.bottom) // Указываем что верх нашего historyTableView будет привязан к низу clearButton
+            make.bottom.equalToSuperview() // Указываем что снизу наш historyTableView будет равен родителю
         }
     }
     
 }
 
-extension HistoryController: HistoryCellDelegate {
-    func onClick(model: HistoryModel) {
-        viewModel.selecthistory(model: model)
+extension HistoryController: HistoryCellDelegate { // Используем расширение для использования делегата HistoryCellDelegate
+    func onClick(model: HistoryModel) {  // Используем функцию которая будет срабатывать при нажатии на ячейку
+        viewModel.selecthistory(model: model) // Указываем что выбранная клетка будет соответствовать нашей модели указанной при использовании функции
         
-        self.navigationController?.popViewController(animated: true)
+        self.navigationController?.popViewController(animated: true) // Указываем что будет совершен переход на исходный(Main) контроллер с анимацией
     }
 }
 
-extension HistoryController: HistoryDelegate {
-    func showHistory(model: Results<HistoryModel>) {
-        historyTableView.fill(models: model)
+extension HistoryController: HistoryDelegate { // Используем расширение для использования делегата HistoryDelegate
+    func showHistory(model: Results<HistoryModel>) { // Используем функцию которая будет принимать в себе результаты из Базы Данных по нашей модели
+        historyTableView.fill(models: model) // Указываем что наш tableView будет заполнен по модели указанной при использовании функции
     }
 }
--- Calculator/UI/History/HistoryViewModel.swift
+++ Calculator/UI/History/HistoryViewModel.swift
@@ -8,33 +8,36 @@
 import Foundation
 import RealmSwift
 
+// Создаём протокол(обязательное условие при его использовании) который будет содержать в себе одну обязательную при использовании функцию
 protocol HistoryDelegate: AnyObject {
-    func showHistory(model: Results<HistoryModel>)
+    func showHistory(model: Results<HistoryModel>) // Указываем функцию которая будет показывать результаты из Базы Данных по нашей модели
 }
 
+// Создаём нашу HistoryViewModel которая будет отвечать за логику между контроллером и UI-элементаим
 class HistoryViewModel {
     
-    private weak var delegate: HistoryDelegate? = nil
+    private weak var delegate: HistoryDelegate? = nil  // Создаём переменную weak(чтобы даже если что-то произойдёт эта переменная продолжала существовать) которая будет давать нам доступ к методам делегата с параметром private
     
-    weak var historyDelegate: HistorySelectDelegate? = nil
+    weak var historyDelegate: HistorySelectDelegate? = nil   // Создаём переменную weak(чтобы даже если что-то произойдёт эта переменная продолжала существовать) которая будет давать нам доступ к методам делегата
     
-    init(delegate: HistoryDelegate) {
-        self.delegate = delegate
+    init(delegate: HistoryDelegate) { // Создаём инициализатор который будет в себе принимать протокол
+        self.delegate = delegate// Протокол созданный ранее будет равен указанному при использовании переменной
+
     }
     
-    func selecthistory(model: HistoryModel) {
-        historyDelegate?.selectHistory(model: model)
+    func selecthistory(model: HistoryModel) { // Создаём функцию которая будет в себе принимать нашу модель
+        historyDelegate?.selectHistory(model: model) // Указываем что функция прописанная ранее будет принимать в себе модель указанную при использовании этой функции
     }
     
-    func deleteAllHistory() {
-        DataBase.shared.deleteAllHistory()
+    func deleteAllHistory() { // Создаём функцию которая будет удалять всю историю
+        DataBase.shared.deleteAllHistory() // Используем функцию deleteAllHistory для удаления которую мы создали в классе DataBase
         
-        getHistory()
+        getHistory() // Используем функцию созданную ниже
     }
     
-    func getHistory() {
-        if let history = DataBase.shared.getHistory() {
-            delegate?.showHistory(model: history)
+    func getHistory() { // Создаём функцию которая будет запрашивать данные из Базы Данных и обновлять данные в tableView
+        if let history = DataBase.shared.getHistory() { // Указываем что если функция getHistory существует
+            delegate?.showHistory(model: history) // Используя делегат показываем историю по модели 
         }
     }
 }
